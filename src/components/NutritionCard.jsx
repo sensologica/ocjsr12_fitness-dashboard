6400e58498aff0cc6d9a39dae9cc47d4642e1e21
sensologica <119978540+sensologica@users.noreply.github.com>
2023-09-28 12:05:19 +0200

    Build Nutrition Card component

---
+++ src/components/NutritionCard.jsx
@@ -0,0 +1,51 @@
+import iconCalories from "../assets/images/icon_calories.svg";
+import iconProteins from "../assets/images/icon_proteins.svg";
+import iconCarbs from "../assets/images/icon_carbs.svg";
+import iconFats from "../assets/images/icon_fats.svg";
+import "./NutritionCard.css";
+
+export default function NutritionCard({type, value}) {
+  const types = [
+    {
+      "type": "calories",
+      "icon": iconCalories,
+      "unit": "kCal",
+      "label": "Calories"
+    },
+    { 
+      "type": "proteins",
+      "icon": iconProteins,
+      "unit": "g",
+      "label": "Proteines"
+    }, 
+    {
+      "type": "carbs",
+      "icon": iconCarbs,
+      "unit": "g",
+      "label": "Glucides"
+    }, 
+    {
+      "type": "fats",
+      "icon": iconFats,
+      "unit": "g",
+      "label": "Lipides"
+    }
+  ];
+  
+  const thisType = types.find(item => item.type === type);
+  console.log(thisType.icon)
+
+  return (
+    <div className="nutrition-card">
+      <div className="nutrition-card__flex-child">
+        <img className="nutrition-card__icon" src={thisType.icon} alt="" />
+      </div>
+      <div className="nutrition-card__flex-child">
+        <p className="nutrition-card__value">
+          {new Intl.NumberFormat("en-US").format(value) + thisType.unit}
+        </p>
+        <p className="nutrition-card__label">{thisType.label}</p>
+      </div>
+    </div>
+  );
+}
