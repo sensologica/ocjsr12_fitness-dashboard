b49abaf03d3c1a0abd3597ba5805f03d334c39a8
sensologica <119978540+sensologica@users.noreply.github.com>
2023-11-03 10:21:27 +0100

    Add element that shows user's name on dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -84,35 +84,43 @@ export default function Dashboard() {
 
   return (
     <div className="dashboard">
-      <WidgetActivity data={data.activity} />
-      <WidgetSessions data={data.performance} />
-      <WidgetPerformance data={data.performance} />
-      <WidgetTodayScore
-        value={
-          data.profile &&
-          (data.profile.data.todayScore || data.profile.data.score)
-        }
-      />
-      <WidgetNutritionCard
-        className="widget-nutrition-card__calories"
-        type="calories"
-        value={data.profile && data.profile.data.keyData.calorieCount} 
-      />
-      <WidgetNutritionCard
-        className="widget-nutrition-card__proteins"
-        type="proteins"
-        value={data.profile && data.profile.data.keyData.proteinCount}
-      />
-      <WidgetNutritionCard
-        className="widget-nutrition-card__carbs"
-        type="carbs"
-        value={data.profile && data.profile.data.keyData.carbohydrateCount}
-      />
-      <WidgetNutritionCard
-        className="widget-nutrition-card__fats"
-        type="fats"
-        value={data.profile && data.profile.data.keyData.lipidCount}
-      />
+      <div className="dashboard-intro">
+        <h1>Bonjour <span className="dashboard-intro__user-name">
+          {data.profile && data.profile.data.userInfos.firstName}
+        </span></h1>
+        <p>Félicitations ! Vous avez explosé vos objectifs hier 👏</p>
+      </div>
+      <div className="widgets">
+        <WidgetActivity data={data.activity} />
+        <WidgetSessions data={data.performance} />
+        <WidgetPerformance data={data.performance} />
+        <WidgetTodayScore
+          value={
+            data.profile &&
+            (data.profile.data.todayScore || data.profile.data.score)
+          }
+        />
+        <WidgetNutritionCard
+          className="widget-nutrition-card__calories"
+          type="calories"
+          value={data.profile && data.profile.data.keyData.calorieCount} 
+        />
+        <WidgetNutritionCard
+          className="widget-nutrition-card__proteins"
+          type="proteins"
+          value={data.profile && data.profile.data.keyData.proteinCount}
+        />
+        <WidgetNutritionCard
+          className="widget-nutrition-card__carbs"
+          type="carbs"
+          value={data.profile && data.profile.data.keyData.carbohydrateCount}
+        />
+        <WidgetNutritionCard
+          className="widget-nutrition-card__fats"
+          type="fats"
+          value={data.profile && data.profile.data.keyData.lipidCount}
+        />
+      </div>
     </div>
   )
 }
